b06276395d7c8aef71c12f54fc4941cad87bb70d
Irene Fuente Romero <irene.fuente@ultimasoftware.com>
2022-05-31 17:12:55 +0200

    guardo semillas encriptadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,37 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { Inject } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
-
 import * as Mnemonic from "bitcore-mnemonic";
 import * as CryptoJS from "crypto-js";
-import { hdkey } from "ethereumjs-wallet";
-import * as bip39 from "bip39";
-import * as util from "ethereumjs-util";
-import Web3 from 'web3';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
 
+// seeds ex
+// february current defy one inform wet hurry cupboard type enable spare famous
+
+export class AppComponent {
+    loginForm: any;
+
+    constructor(private formBuilder: FormBuilder) {
+        this.loginForm = this.formBuilder.group({
+            seeds: '',
+            password: ''
+        });
+    }
+
+    sendLogin (loginData: any) {
+        const { seeds, password } = loginData;
+        if(!seeds || !password)
+            return alert('Please, enter your credentials');
+
+        const validSeeds = Mnemonic.isValid(seeds);
+        if(!validSeeds)
+            return alert('Invalid seeds');
+
+        const encryptedSeeds = CryptoJS.AES.encrypt(seeds, password).toString();
+        localStorage.setItem('seeds', encryptedSeeds);
+    }
 }
