474d469ac522e22defcc35a671bebfc54dc7e9b2
Irene Fuente Romero <irene.fuente@ultimasoftware.com>
2022-05-31 18:01:09 +0200

    al hacer login muestro la address

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import * as Mnemonic from "bitcore-mnemonic";
 import * as CryptoJS from "crypto-js";
+import * as bip39 from "bip39";
+import { hdkey } from "ethereumjs-wallet";
+import * as util from "ethereumjs-util";
 
 @Component({
     selector: 'app-root',
@@ -15,6 +18,9 @@ import * as CryptoJS from "crypto-js";
 export class AppComponent {
     loginForm: any;
     encrypted: any;
+    wallet: any = {
+        address: ''
+    }
 
     constructor(private formBuilder: FormBuilder) {
         this.loginForm = this.formBuilder.group({
@@ -34,6 +40,7 @@ export class AppComponent {
             const decrypted = CryptoJS.AES.decrypt(this.encrypted, password).toString(CryptoJS.enc.Utf8);
             seeds = decrypted;
             this.loginForm.reset();
+            this.initializeWallet(seeds);
         } else {
             if(!password || !seeds)
                 return alert('Please, enter your credentials');
@@ -44,7 +51,21 @@ export class AppComponent {
             const encryptedSeeds = CryptoJS.AES.encrypt(seeds, password).toString();
             localStorage.setItem('seeds', encryptedSeeds);
         }
+    }
 
+    async initializeWallet(seeds: string){
+        const mnemonic = new Mnemonic(seeds);
+        const seed = await bip39.mnemonicToSeed(mnemonic.toString());
+        const path = "m/44'/60'/0'/0/0";
 
+        const wallet = hdkey
+            .fromMasterSeed(seed)
+            .derivePath(path)
+            .getWallet();
+
+        const privateKey = wallet.getPrivateKey();
+        const publicKey = util.privateToPublic(privateKey);
+        const address = "0x" + util.pubToAddress(publicKey).toString("hex");
+        this.wallet.address = address;
     }
 }
